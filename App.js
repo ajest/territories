0021025b20eba8c3f12b05757c3f8884953d8720
Pablo Fumarola <pablo.fumarola@gmail.com>
2020-07-12 14:24:39 -0300

    Added some libraries

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header, Card } from 'react-native-elements';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Header
+        leftComponent={{ icon: 'menu', color: '#fff' }}
+        centerComponent={{ text: 'Territorios', style: { color: '#fff' } }}
+        ViewComponent={LinearGradient}
+        linearGradientProps={{
+          colors: ['orange', 'red'],
+          start: { x: 0.9, y: 2 },
+          end: { x: 1, y: 0 },
+        }}
+      />
+      <View>
+        <Card title="Mapa general">
+          <Text>Aquí estará el mapa general</Text>
+        </Card>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
