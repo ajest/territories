b0a119ff1515fa0260519e0b1301512c4516e357
Pablo Fumarola <pablo.fumarola@gmail.com>
2020-07-15 18:55:13 -0300

    Default Region in Buenos Aires

--- components/Map.js
+++ components/Map.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
-const Map = () => (
-  <View style={styles.container}>
-    <MapView
-      style={styles.mapStyle}
-      zoomEnabled={true}
-      showsUserLocation={true}
-    >
-      <Marker
-        coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-        title={'title'}
-        description={'description'}
-      />
-    </MapView>
-  </View>
-);
+const defaultRegion = {
+  latitude: -34.6158037,
+  longitude: -58.5033386,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
+
+class Map extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.mapStyle}
+          showsUserLocation={true}
+          region={defaultRegion}
+          zoomEnabled={true}
+          zoomControlEnabled={true}
+        >
+          <Marker
+            coordinate={{ latitude: -34.6158037, longitude: -58.5033386 }}
+            title={'Supermercado'}
+            description={'Avenida Gaona 2120'}
+          />
+        </MapView>
+      </View>
+    );
+  }
+}
 
 export default Map;
 
